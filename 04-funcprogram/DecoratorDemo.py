7b681c3b07513941cdd7093c3442ab796bd9b280
dengzz <707643733@qq.com>
2022-05-09 10:54:14 +0800

    .

--- 04-funcprogram/DecoratorDemo.py
+++ 04-funcprogram/DecoratorDemo.py
@@ -3,6 +3,7 @@
 # @Time      :2022/5/7 17:48
 # @Author    :yhl
 # @Function  :
+from datetime import datetime
 import functools
 import time
 
@@ -45,4 +46,15 @@ def fast(x, y):
 
 
 if __name__ == "__main__":
+    dt_now = datetime.fromtimestamp(time.time())
+    print(dt_now)
+    print(dt_now.strftime('%Y%m%d'))
     print(fast(1, 2))
+    time_str = '2020-06-16 10:31:08'
+    dt_time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')  # 从字符串向datetime转化时，必须根据字符串的格式来订单strptime的第二个参数format
+    print(dt_time)
+    time_str1 = '2020/10/17'
+    dt_time1 = datetime.strptime(time_str1, '%Y/%m/%d')
+    print(dt_time1)  # 2020-06-17 00:00:00
+
+    print(dt_time1.strftime("%Y/%#m/%d"))  # 字符串转为日期型 月份如果是单月份 前面不会有0
